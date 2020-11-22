234887218534640de692bb2f372c7ca9166e6fef
brian4548 <brianr4548@gmail.com>
2020-11-22 16:21:49 -0600

    commit tree 10ebfe6a32cbc6bbe1caa876886676cedd5bdbe2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+require 'pry'
+
+
+class School
+
+    attr_accessor :school, :roster
+
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student (name, grade)
+        if roster[grade]
+            roster[grade] << name
+        else roster[grade] = []
+            roster[grade] << name
+        end 
+        roster
+    
+    end 
+
+
+    def grade (grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, name|
+            name.sort!
+        end
+    end
+
+
+    
+
+end
+    
+
+
